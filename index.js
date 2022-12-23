d9a5798763a780b74b013ec89b250fccacecf34a
LonelyMitoc <jamesmatsu@gmail.com>
2022-12-22 21:04:58 -0800

    index.js for main is good

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const db = require('./config/connection');
+const db = require('./config/connection');
 // const routes = require('./routes');
 
 const PORT = process.env.port || 3001;
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
-// db.once('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}`);
   });
-// });
+});
