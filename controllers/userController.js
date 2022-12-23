f9d2edbb9fc6b6f60adb47b454681e3909ba35e4
LonelyMitoc <jamesmatsu@gmail.com>
2022-12-22 22:09:30 -0800

    user contorller: getUsers, getSingleUser, createUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,37 @@
-const { User, Thought} = require('../models');
+const { User, Thought } = require('../models');
+
+async function getUsers(req, res) {
+  try {
+    const users = await User.find();
+    return res.json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+
+async function getSingleUser(req, res) {
+  try {
+    const user = await User.findOne(req.params.id);
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+
+async function createUser(req, res) {
+  try {
+    const users = await User.create(req.body);
+    return res.json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
 
 module.exports = {
-  async function getUsers(req,res) {
-    try {
-      const users = await User.find();
-      return res.json(users);
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
-  },
-  async function createUser(req, res) {
-    try {
-      const users = await User.create(req.body);
-      return res.json(users);
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
-  }
-}
+  getUsers,
+  getSingleUser,
+  createUser,
+};
